ec78edb52266da7f32020480b9bcc2360e65f846
seyed1383 <66937191+seyed1383@users.noreply.github.com>
2023-08-01 01:01:57 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x5f459b92c5e28a24682d8d68558dfab58aa6c145";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
